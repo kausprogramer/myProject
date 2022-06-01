9dc35a4bcc941406f26d4068b1d8a085fbdd4dbe
kausprogramer <kos2bh@gmail.com>
2022-06-01 18:34:28 +0530

    commit

--- App.js
+++ App.js
@@ -1,24 +1,39 @@
-import { Button, StyleSheet, Text, View,TextInput } from 'react-native';
+import { Button, StyleSheet, Text, View,ScrollView, FlatList,TouchableOpacity } from 'react-native';
 import {useState} from "react";
 
 export default function App() {
   const [people,setPeople]= useState([
-    {name:'bunty', key:'1'},
-    {name:'keshav', key:'2'},
-    {name:'kaustubh', key:'3'},
-    {name:'karan', key:'4'},
-    {name:'Vibhu', key:'5'},
-  ])
+    {name:'bunty', id:'1'},
+    {name:'keshav', id:'2'},
+    {name:'kaustubh', id:'3'},
+    {name:'karan', id:'4'},
+    {name:'Vibhu', id:'5'},
+    {name:'Kartik', id:'6'},
+    {name:'lokesh', id:'7'},
+    {name:'vinny', id:'8'},
+  ]);
+
+  const pressHandler=(id)=>{
+    console.log(id);
+    setPeople((prevPeople)=>{
+      return prevPeople.filter(person=>person.id!=id)
+    }
+    )
+    }
+
   return (
     <View style={styles.container}>
-      {people.map(element => {
-        return (
-          <View key={(element.key)}>
-            <Text>{element.name}</Text>
-          </View>
+      <FlatList
+      numColumns={2}
+      keyExtractor={(item)=>item.id}
+        data={people}
+        renderItem={({item})=>(
+          <TouchableOpacity onPress={() => pressHandler(item.id)}>
+          <Text style={styles.element}>{item.name}</Text>
+          </TouchableOpacity>
         )
-      }
-      )}
+        }
+      />
     </View>
   );
 }
@@ -31,5 +46,12 @@ const styles = StyleSheet.create({
     paddingHorizontal:20,
     // alignItems: 'center',
     // justifyContent: 'center',
+  },
+  element:{
+    marginTop:24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize:24,
+    marginHorizontal:10,
   }
 });
